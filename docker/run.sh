1f26539ff96269fd99542318ec19b5240c1c941b
Emmanuel S <5435148+esgn@users.noreply.github.com>
2022-04-11 09:36:22 +0200

    Update run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,1 +1,1 @@
-docker run -v /mnt/chantier_nualid:/input -v /mnt/output_dir:/output --rm --entrypoint /bin/bash -it pointstools
+docker run -v /mnt/chantier_nualid/data:/input -v /mnt/output_dir:/output --rm --entrypoint /bin/bash -it pointstools
