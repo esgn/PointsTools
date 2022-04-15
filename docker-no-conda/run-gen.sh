f4d4a12589b81d74f8722dd3984216dac8f67369
Emmanuel S <5435148+esgn@users.noreply.github.com>
2022-04-15 12:51:03 +0200

    Update run-gen.sh

--- docker-no-conda/run-gen.sh
+++ docker-no-conda/run-gen.sh
@@ -1,1 +1,1 @@
-docker run -v /mnt/input/data:/input -v /mnt/output:/output -it pointstools-no-conda /bin/bash -c "date > /output/start.txt && node ./src/index.js configFile.js && date > /output/finish.txt"
+docker run -v /mnt/input/data:/input -v /mnt/output:/output -it pointstools-no-conda /bin/bash -c "date +%s > /output/start.txt && node ./src/index.js configFile.js && date +%s > /output/finish.txt"
