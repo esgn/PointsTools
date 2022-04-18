7d97d257c4df0ef4d3f24ed50a9b63fc564bad3f
Emmanuel S <5435148+esgn@users.noreply.github.com>
2022-04-18 17:06:52 +0200

    Update run-gen.sh
    redirect output and error to files

--- docker-no-conda/run-gen.sh
+++ docker-no-conda/run-gen.sh
@@ -1,1 +1,1 @@
-docker run -v /mnt/input/data:/input -v /mnt/output:/output -it pointstools-no-conda /bin/bash -c "date +%s > /output/start.txt && node ./src/index.js configFile.js && date +%s > /output/end.txt"
+docker run -v /mnt/input/data:/input -v /mnt/output:/output -it pointstools-no-conda /bin/bash -c "date +%s > /output/start.txt && node ./src/index.js configFile.js > /output/generation.log 2>/output/generation.error && date +%s > /output/end.txt"
